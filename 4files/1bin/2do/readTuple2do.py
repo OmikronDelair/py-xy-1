f9a287f86404d4ee2111123255c63c3fada6255e
Leonardo Gonzalez <omikron.dementia@gmail.com>
2022-05-02 22:49:15 -0500

    Added 3rd homework: write/read tuple .bin files

--- 4files/1bin/2do/readTuple2do.py
+++ 4files/1bin/2do/readTuple2do.py
@@ -1,4 +1,13 @@
-#
 # read binary file (tuple)
 # and print it
+
+import pickle
+
+with open('tuple.bin','rb') as fh:
+        t = pickle.load(fh) 
+
+print(type(t))
+print(t)
+
+print('done...')
 #
--- 4files/1bin/2do/writeTuple2do.py
+++ 4files/1bin/2do/writeTuple2do.py
@@ -2,4 +2,11 @@
 # 2do
 #
 
+import pickle
+
 t=12,True,3.1,'aCat'
+
+with open('tuple.bin','wb') as fh:
+        pickle.dump(t,fh)
+
+print('done...')
